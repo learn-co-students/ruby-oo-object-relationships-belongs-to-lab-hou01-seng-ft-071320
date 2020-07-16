b3749442d2b7d910b87b27e4af233cff8318a1c6
kirirotha <kirirotha@gmail.com>
2020-07-15 19:57:15 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,14 @@
+class Artist
+
+    attr_accessor :name
+    def initialize
+        @name = name
+    end
+    @name
+    
+end
+
+
+#artist = Artist.new
+#artist.name = "Drake"
+#puts artist.name
---
+++ lib/author.rb
@@ -0,0 +1,9 @@
+class Author
+    attr_accessor :name
+
+    def iniitialize(name)
+        @name = name
+    end
+
+    
+end 
